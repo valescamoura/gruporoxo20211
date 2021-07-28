3a02f5d20edef01af0d9c0b5fdd524146618dc74
valescamoura <valescamoura@id.uff.br>
2021-07-27 23:43:36 -0300

    Adicionando flame e base para as classes do jogo.

---
+++ lib/blackjack/Carta.dart
@@ -0,0 +1,12 @@
+class Carta {
+  // Atributos
+  String naipe;
+  int valor;
+  String url;
+
+  // Construtor
+  Carta(this.naipe, this.valor, this.url);
+
+  // Métodos
+
+}
---
+++ lib/blackjack/Jogador.dart
@@ -0,0 +1,15 @@
+import 'package:gruporoxo20211/blackjack/Carta.dart';
+
+class Jogador {
+  // Atributos
+  Carta mao;
+  int pontos;
+
+  // Construtor
+  Jogador(this.mao, this.pontos);
+
+  //Métodos
+  void abaixarMao(Carta mao) {
+    //TODO
+  }
+}
