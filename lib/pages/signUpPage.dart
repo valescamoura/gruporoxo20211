249eef972c39651f8221425f2e71086d9c46ac84
karina288 <karinalemos@id.uff.br>
2021-08-13 18:39:02 -0300

    Inserção do campo nickname no cadastro

--- lib/pages/signUpPage.dart
+++ lib/pages/signUpPage.dart
@@ -28,6 +28,7 @@ class SignUpPage extends StatelessWidget {
 
 class SignUpForm extends StatelessWidget {
   final GlobalKey<FormState> _signUpKey = GlobalKey<FormState>();
+  final _textNickname = TextEditingController();
   final _textEmail = TextEditingController();
   final _textPassword = TextEditingController();
 
@@ -36,21 +37,39 @@ class SignUpForm extends StatelessWidget {
     return Container(
       padding: const EdgeInsets.all(8.0),
       margin: const EdgeInsets.symmetric(vertical: 20, horizontal: 55),
-      height: 310,
+      height: 372,
       color: Color(0xFB126012),
       child: Form(
         key: _signUpKey,
         child: Column(
           children: <Widget>[
             Padding(
-              padding: const EdgeInsets.fromLTRB(10, 20, 10, 0),
+              padding: const EdgeInsets.fromLTRB(10, 15, 10, 0),
+              child: TextFormField(
+                controller: _textNickname,
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  border: OutlineInputBorder(),
+                  labelText: 'Nickname',
+                ),
+                validator: (String? value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Este campo é obrigatório';
+                  }
+                  return null;
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(10, 15, 10, 0),
               child: TextFormField(
                 controller: _textEmail,
                 decoration: InputDecoration(
                     filled: true,
                     fillColor: Colors.white,
                     border: OutlineInputBorder(),
-                    labelText: 'Email',
+                    labelText: 'E-mail',
                     hintText: 'example@gmail.com'),
                 validator: (String? value) {
                   if (value == null || value.isEmpty) {
@@ -61,7 +80,7 @@ class SignUpForm extends StatelessWidget {
               ),
             ),
             Padding(
-              padding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
+              padding: const EdgeInsets.fromLTRB(10, 15, 10, 10),
               child: TextFormField(
                 controller: _textPassword,
                 decoration: InputDecoration(
@@ -80,7 +99,7 @@ class SignUpForm extends StatelessWidget {
               ),
             ),
             Padding(
-              padding: const EdgeInsets.fromLTRB(0, 0, 0, 16),
+              padding: const EdgeInsets.only(top: 10),
               child: ElevatedButton(
                 style: ButtonStyle(
                   shape: MaterialStateProperty.all<RoundedRectangleBorder>(
