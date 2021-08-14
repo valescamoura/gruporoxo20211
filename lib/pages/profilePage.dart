3a572e827d56d3e73e1d09136e80cf0d6dbeeeae
karina288 <karinalemos@id.uff.br>
2021-08-14 20:33:15 -0300

    ajustes na página perfil

--- lib/pages/profilePage.dart
+++ lib/pages/profilePage.dart
@@ -3,13 +3,17 @@ import 'package:gruporoxo20211/AuthenticationService.dart';
 import 'package:provider/provider.dart';
 
 class ProfilePage extends StatelessWidget {
+  const ProfilePage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     String? _name = context.read<AuthenticationService>().getNickname();
     String? _email = context.read<AuthenticationService>().getEmail();
-    int _wins = context.read<AuthenticationService>().getWins();
-    int _losses = context.read<AuthenticationService>().getLosses();
+    //int _wins = context.read<AuthenticationService>().getWins();
+    //int _losses = context.read<AuthenticationService>().getLosses();
+
     return Scaffold(
+        backgroundColor: Color(0xFF062B06),
         appBar: AppBar(
           centerTitle: true,
           title: const Text(
@@ -24,82 +28,84 @@ class ProfilePage extends StatelessWidget {
             },
           ),
         ),
-        body: Card(
-            margin: const EdgeInsets.symmetric(vertical: 20, horizontal: 55),
-            color: Color(0xFB126012),
-            child: Column(
-              children: [
-                Padding(
-                    padding: EdgeInsets.only(top: 20.0),
-                    child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10.0),
-                          color: Color(0xFFAD200C),
-                        ),
-                        padding: EdgeInsets.only(top: 10.0),
-                        width: 120,
-                        height: 100,
-                        child: Column(
-                          children: [
-                            Column(
-                              children: [
-                                Text("Nickname:",
-                                    style: TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 18.0,
-                                        fontWeight: FontWeight.bold)),
-                                Text(_name.toString(),
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 18.0,
-                                    ))
-                              ],
-                            ),
-                            Column(
-                              children: [
-                                Text("Email:",
-                                    style: TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 18.0,
-                                        fontWeight: FontWeight.bold)),
-                                Text(_email.toString(),
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 18.0,
-                                    ))
-                              ],
-                            ),
-                            Column(
-                              children: [
-                                Text("Vitorias:",
-                                    style: TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 18.0,
-                                        fontWeight: FontWeight.bold)),
-                                Text(_wins.toString(),
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 18.0,
-                                    ))
-                              ],
-                            ),
-                            Column(
-                              children: [
-                                Text("Derrotas:",
-                                    style: TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 18.0,
-                                        fontWeight: FontWeight.bold)),
-                                Text(_losses.toString(),
+        body: Container(
+            width: 400,
+            height: 400,
+            child: Padding(
+              padding: EdgeInsets.only(top: 30.0, left: 30.0),
+              child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  color: Color(0xFB126012),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                          padding: EdgeInsets.only(left: 20.0, top: 30.0),
+                          child: Text("Nickname:",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.bold))),
+                      Padding(
+                          padding: EdgeInsets.only(left: 20.0, top: 10.0),
+                          child: Text(_name.toString(),
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20.0,
+                              ))),
+                      Padding(
+                          padding: EdgeInsets.only(left: 20.0, top: 30.0),
+                          child: Text("E-mail:",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.bold))),
+                      Padding(
+                          padding: EdgeInsets.only(top: 10.0, left: 20.0),
+                          child: Text(_email.toString(),
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20.0,
+                              ))),
+                      Padding(
+                          padding: EdgeInsets.only(top: 30.0, left: 15.0),
+                          child: Row(children: [
+                            Image.asset("assets/images/imageTrophy.png"),
+                            Text("Vitórias:",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 20.0,
+                                    fontWeight: FontWeight.bold))
+                          ])),
+                      Padding(
+                          padding: EdgeInsets.only(top: 10.0, left: 20.0),
+                          child: Text(
+                              "" /*_wins.toString(),
                                     style: TextStyle(
                                       color: Colors.white,
-                                      fontSize: 18.0,
-                                    ))
-                              ],
-                            ),
-                          ],
-                        ))),
-              ],
+                                      fontSize: 20.0,
+                                    )*/
+                              )),
+                      Padding(
+                          padding: EdgeInsets.only(top: 20.0, left: 20.0),
+                          child: Text("Derrotas:",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.bold))),
+                      Padding(
+                          padding: EdgeInsets.only(top: 10.0, left: 20.0),
+                          child: Text(
+                              "" /*_losses.toString(),
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 20.0,
+                                    )*/
+                              ))
+                    ],
+                  )),
             )));
   }
 }
